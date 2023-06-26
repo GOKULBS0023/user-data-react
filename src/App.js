659872fa11629bd84a2b727fc0fe21e3bd3815be
Gokulbharathi Sivasamy <gokulbs0023@gmail.com>
2023-06-26 12:39:33 +0530

    Add Wrapper Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import UserList from "./Components/JS/UserList";
 import ErrorModal from "./Components/JS/ErrorModal";
 import AddUser from "./Components/JS/AddUser";
+import Wrapper from "./Components/Helpers/Wrapper"
 
 function App() {
   const [userData, setUserData] = useState([]);
@@ -21,11 +22,11 @@ function App() {
     setValid(true);
   }
   return (
-    <div>
+    <Wrapper>
       <AddUser onReturn={getData} onValidation={getValidation}/>
       {userData.length > 0 && <UserList data={userData}/>}
       {!valid &&  <ErrorModal onAccept={getAccept} title={"Invalid Data"} message={"Please enter the valid data"} hintOne={hint1} hintTwo={hint2}/>}
-    </div>
+    </Wrapper>
   );
 }
 
